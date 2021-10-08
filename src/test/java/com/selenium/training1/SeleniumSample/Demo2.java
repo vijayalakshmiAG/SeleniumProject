37dd3d0c6bdca2748148290bfc6a186ed66db25c
00004884 <00004884@AGI-LT-786.arisglobal.com>
2021-10-08 13:44:30 +0530

    First commit

---
+++ src/test/java/com/selenium/training1/SeleniumSample/Demo2.java
@@ -0,0 +1,10 @@
+package com.selenium.training1.SeleniumSample;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Hello");
+	}
+
+}
